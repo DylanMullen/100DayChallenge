7bbbfa7901186feea7b006a867afba2021de6e25
Dylan Mullen <31774413+DylanMullen@users.noreply.github.com>
2022-02-03 21:47:41 +0000

    Initial Commit
    Backend Started

---
+++ 100 Day Challenge - Backend/src/main/java/me/dylanmullen/weightchallenge/Runner.java
@@ -0,0 +1,11 @@
+package me.dylanmullen.weightchallenge;
+
+public class Runner
+{
+
+	public static void main(String[] args)
+	{
+		
+	}
+	
+}
---
+++ 100 Day Challenge - Backend/src/main/java/me/dylanmullen/weightchallenge/core/App.java
@@ -0,0 +1,6 @@
+package me.dylanmullen.weightchallenge.core;
+
+public class App
+{
+
+}
